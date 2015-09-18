65ac3a3b15e69ea590edb9ceca2bba0e9097c2ae
Ayush Aggarwal <ayush.agarwal7689@gmail.com>
2015-09-18 15:49:37 +0530

    probablity fixed final

--- smart/retrieval_neo4j.py
+++ smart/retrieval_neo4j.py
@@ -1,19 +1,14 @@
 #MATCH (n:Haryana)-[r]->(m) where type(r)=~ '.*minister.*' return m
 import json
+import re
 from py2neo import Graph,authenticate,Node,Relationship
-<<<<<<< HEAD
-authenticate("ip:port","username","password")
+authenticate("ip:port","user","pwd")
 graph=Graph("db path")
 def findabbrword(query):
 	query=query.strip().upper()
 	n=graph.cypher.execute("MATCH (n:`"+query+"`)-[r:`abbrevation`]->(m) RETURN m.name")
 	n=n[0][0]
 	return n
-=======
-authenticate("ip","neo4j","password")
-graph=Graph()
-
->>>>>>> 2e867e4eb5908876ac4fe7422f7197701234a47b
 def reg_search(query):
 	l=[]
 	query=query.replace(" of "," ")
@@ -48,7 +43,7 @@ def reg_search(query):
 	#res=graph.cypher.execute(neo_query)
 	for i in range(0,len(rquery)):
 		ans=[]
-		neo_query1="match (n) where n.name=~ '(?i).*%s.*' return n"%(rquery[i])
+		neo_query1="match (n) where n.name=~ '(?i).*%s.*' and toFloat(toFloat(%s)/(n.len))>=0.5 return n"%(rquery[i],len(rquery))
 		#neo_query2="MATCH (n)-[r]->(m) where type(r)=~ '.*%s.*' return m"%(query[i])
 		#res=graph.cypher.execute(neo_query1)
 		#ans.append(res)
@@ -117,67 +112,69 @@ def reg_search(query):
 	l=json.dumps(l)
 	print l
 				
-query=raw_input("Query? ")
-orig_query=query
-result={}
-#print query
-query=query.replace(" of "," ").replace("highway","").replace("river","").replace("university","").strip()
-if len(query)==2 or len(query)==3:
-	try:
-		tem=query
-		query=findabbrword(query)
-	except:
-		query=tem
-neo_query="match (n) where n.name=~ '(?i)%s' return n"%(query)	
-res=graph.cypher.execute(neo_query)
-if len(res)!=0:	
-	#print res
-	nnode=res[0][0]
-	result["properties"]=nnode.properties
-	rquery="match (n)-[r]->(m) where n.name=~ '(?i)"+nnode["name"]+"' return r"
-	res1=graph.cypher.execute(rquery)
-	#print res1
-	for j in res1:
-		rtype=j[0].type
-		#print rtype
-		if (result.has_key(rtype)==False):
-			nquery="match (n)-[r]->(m) where n.name=~ '(?i)"+nnode["name"]+"' and type(r)= '"+rtype+"' return m,r"
-			res2=graph.cypher.execute(nquery)
-			#nquery1="match (n)-[r]->(m) where n.name=~ '(?i)"+nnode["name"]+"' and type(r)=~ '(?i)"+rtype+"' return r"
-			#res3=graph.cypher.execute(nquery1)
-			#print nquery,res2
-			if len(res2)>1:
-				ilist=[]
-				for k in res2:
-					idata=str((k[0]["name"]))
-					iprop=filter(lambda x:ord(x)>31 and ord(x)<128,str(k[1].properties))
-					if len(iprop)!=0:
-						u=[]
-						u.append(str(idata))
-						u.append(k[1].properties)
-						ilist.append(u)
-					else:
-						ilist.append([str(idata)])
+def main(query):
+	orig_query=query
+	result={}
+	#print query
+	query=query.replace("national highway ","nh")
+	query=re.sub(r'nh\s','nh',query)
+	query=query.replace(" of "," ").replace("highway","").replace("river","").replace("university","").strip()
+	if len(query)==2 or len(query)==3:
+		try:
+			tem=query
+			query=findabbrword(query)
+		except:
+			query=tem
+	neo_query="match (n) where n.name=~ '(?i)%s' return n"%(query)	
+	res=graph.cypher.execute(neo_query)
+	if len(res)!=0:	
+		#print res
+		nnode=res[0][0]
+		result["properties"]=nnode.properties
+		rquery="match (n)-[r]->(m) where n.name=~ '(?i)"+nnode["name"]+"' return r"
+		res1=graph.cypher.execute(rquery)
+		#print res1
+		for j in res1:
+			rtype=j[0].type
+			#print rtype
+			if (result.has_key(rtype)==False):
+				nquery="match (n)-[r]->(m) where n.name=~ '(?i)"+nnode["name"]+"' and type(r)= '"+rtype+"' return m,r"
+				res2=graph.cypher.execute(nquery)
+				#nquery1="match (n)-[r]->(m) where n.name=~ '(?i)"+nnode["name"]+"' and type(r)=~ '(?i)"+rtype+"' return r"
+				#res3=graph.cypher.execute(nquery1)
+				#print nquery,res2
+				if len(res2)>1:
+					ilist=[]
+					for k in res2:
+						idata=str((k[0]["name"]))
+						iprop=filter(lambda x:ord(x)>31 and ord(x)<128,str(k[1].properties))
+						if len(iprop)!=0:
+							u=[]
+							u.append(str(idata))
+							u.append(k[1].properties)
+							ilist.append(u)
+						else:
+							ilist.append([str(idata)])
 				
 					
-				result[rtype]=ilist
-			else:
-				temp=res2[0][1].properties
-				if len(temp)!=0:
-					l=[]
-					l.append(str(res2[0][0]["name"]))
-					l.append(res2[0][1].properties)
-					result[rtype]=l 
+					result[rtype]=ilist
 				else:
-					result[rtype]=[str(res2[0][0]["name"])]			
-				#res2=list(set(res2))
+					temp=res2[0][1].properties
+					if len(temp)!=0:
+						l=[]
+						l.append(str(res2[0][0]["name"]))
+						l.append(res2[0][1].properties)
+						result[rtype]=l 
+					else:
+						result[rtype]=[str(res2[0][0]["name"])]			
+					#res2=list(set(res2))
 
-	#print res2
-			#for k in res3:
-			#		iprop=str((k[0].properties)).encode("utf-8")
-			#		print iprop
-	result=json.dumps([result])
-	print result
+		#print res2
+				#for k in res3:
+				#		iprop=str((k[0].properties)).encode("utf-8")
+				#		print iprop
+		result=json.dumps([result])
+		print result
 		
-else:
-	reg_search(orig_query)
+	else:
+		reg_search(orig_query)
